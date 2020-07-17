5afe0ed97b9229c51fb35381e983599deb48b13c
Sergei Morozov <morozserge1st@gmail.com>
2020-07-17 19:10:52 +0300

    Updates

---
+++ precache-manifest.58b858efd52ffa27d0ba7ecd248299a5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cf77a3ea732d4ac3db85d55ebfe138",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "872cf2c267a72b795570",
+    "url": "/portfolio/static/css/main.b8028792.chunk.css"
+  },
+  {
+    "revision": "2e48dad0474b25d2ec2a",
+    "url": "/portfolio/static/js/2.c09bae53.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/portfolio/static/js/2.c09bae53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872cf2c267a72b795570",
+    "url": "/portfolio/static/js/main.2cd06136.chunk.js"
+  },
+  {
+    "revision": "2dd5f6de9cfc7b38ea49",
+    "url": "/portfolio/static/js/runtime-main.8e279039.js"
+  },
+  {
+    "revision": "e72fdf1cca2cebcbe91325bbe9f9e5da",
+    "url": "/portfolio/static/media/RobotoMono-Bold.e72fdf1c.ttf"
+  },
+  {
+    "revision": "fa8ab495d494eccb28f4431f054ddbd4",
+    "url": "/portfolio/static/media/RobotoMono-Light.fa8ab495.ttf"
+  },
+  {
+    "revision": "8ad82b1dc550319993a7d6c932b2656d",
+    "url": "/portfolio/static/media/RobotoMono-Medium.8ad82b1d.ttf"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "/portfolio/static/media/RobotoMono-Regular.e5ca8c0a.ttf"
+  },
+  {
+    "revision": "2a12618b6d46fd798157e4b9d29cdf06",
+    "url": "/portfolio/static/media/RobotoMono-SemiBold.2a12618b.ttf"
+  }
+]);
